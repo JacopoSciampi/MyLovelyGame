4d33b33ec570e08d41d35bdb048a635dbc921f4a
Jacopo Sciampi <sciampi.jacopo@gmail.com>
2021-03-12 12:53:07 +0100

    feat(main-scene): Add content

---
+++ Assets/Scripts/MainMenu/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [Header("Required")]
+    public AudioSource myAudio;
+    public string audioPath;
+    public bool playOnAwake;
+
+    [Header("Optionals")]
+    public bool loopTrack;
+    private void Awake()
+    {
+        if(playOnAwake)
+        {
+            PlaySound();
+        }
+    }
+
+    public void PlaySound()
+    {
+        myAudio.clip = Resources.Load<AudioClip>(audioPath);
+        myAudio.loop = loopTrack;
+
+        myAudio.Play();
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
