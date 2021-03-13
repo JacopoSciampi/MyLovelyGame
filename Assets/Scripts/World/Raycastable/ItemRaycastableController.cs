02d1041709fb420531cc134ab03f9d31b4e7fbf8
Jacopo Sciampi <sciampi.jacopo@gmail.com>
2021-03-13 14:52:40 +0100

    Add Raycast | Add tooltip

---
+++ Assets/Scripts/World/Raycastable/ItemRaycastableController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRaycastableController : MonoBehaviour
+{
+    [Header("Required")]
+    public string itemName;
+
+    public bool canBeDestroyed;
+    public bool showInteractTooltip;
+    public bool canRespwan;
+
+    public string tooltipText;
+    public int lifePoints;
+
+    public void __DEBUG__()
+    {
+        Debug.Log(itemName);
+    }
+
+    public void Damage(int points)
+    {
+        lifePoints -= points;
+
+        //if can respawn.. do
+    }
+}
---
+++ Assets/Scripts/World/WorldManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public static class WorldManager
+{
+    private static TextMeshProUGUI tooltipText;
+    private static Image tooltipImage;
+
+    public static bool isTooltipActive;
+
+    public static void setTooltip(string message)
+    {
+        if (tooltipImage == null || tooltipText == null)
+        {
+            GameObject image = GameObject.Find("IMG_tooltip");
+            tooltipImage = image.GetComponent<Image>();
+            GameObject text = GameObject.Find("tooltipText");
+            tooltipText = text.GetComponent<TextMeshProUGUI>();
+        }
+
+        if (isTooltipActive == false)
+        {
+            isTooltipActive = true;
+
+            Color tmpColor = tooltipImage.color;
+            tmpColor.a = 1f;
+
+            tooltipImage.color = tmpColor;
+            tooltipText.text = message;
+        }
+    }
+
+    public static void removeTooltip()
+    {
+        if (tooltipImage != null && tooltipText != null)
+        {
+            isTooltipActive = false;
+            Color tmpColor = tooltipImage.color;
+            tmpColor.a = 0f;
+
+            tooltipImage.color = tmpColor;
+            tooltipText.text = null;
+        }
+    }
+}
